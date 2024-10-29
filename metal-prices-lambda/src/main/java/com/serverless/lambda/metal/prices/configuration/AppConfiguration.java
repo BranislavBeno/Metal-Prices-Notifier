8d7ccd1b1afb027206c2102106d41ffc9eae05ad
Branislav Beno <Branislav.Beno.ext@msg.group>
2024-10-29 09:37:33 +0100

    feat: Apply spotless formatter again

--- metal-prices-lambda/src/main/java/com/serverless/lambda/metal/prices/configuration/AppConfiguration.java
+++ metal-prices-lambda/src/main/java/com/serverless/lambda/metal/prices/configuration/AppConfiguration.java
@@ -1,14 +1,14 @@
 package com.serverless.lambda.metal.prices.configuration;
 
-import software.amazon.awssdk.regions.Region;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
 
 @Configuration
 public class AppConfiguration {
 
-   @Bean
-   public SsmParamsProvider ssmParamsProvider() {
-      return new SsmParamsProvider(Region.EU_CENTRAL_1);
-   }
+    @Bean
+    public SsmParamsProvider ssmParamsProvider() {
+        return new SsmParamsProvider(Region.EU_CENTRAL_1);
+    }
 }
