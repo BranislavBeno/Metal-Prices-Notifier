b189e95256f264ee2abf359e6306ddd96610635b
Branislav Beno <brano.beno@gmail.com>
2024-10-30 17:10:05 +0100

    feat: Implement PolicyStatement handling

--- aws-cdk/src/main/java/com/serverless/lambda/cdk/MetalPricesLambdaApp.java
+++ aws-cdk/src/main/java/com/serverless/lambda/cdk/MetalPricesLambdaApp.java
@@ -1,45 +1,50 @@
 package com.serverless.lambda.cdk;
 
+import com.serverless.lambda.cdk.construct.ApplicationEnvironment;
+import com.serverless.lambda.cdk.util.CdkUtil;
+import com.serverless.lambda.cdk.util.Validations;
+import java.util.List;
 import software.amazon.awscdk.App;
 import software.amazon.awscdk.Environment;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.iam.Effect;
 import software.amazon.awscdk.services.iam.PolicyStatement;
-import java.util.List;
-import com.serverless.lambda.cdk.construct.ApplicationEnvironment;
-import com.serverless.lambda.cdk.util.CdkUtil;
-import com.serverless.lambda.cdk.util.Validations;
 
 public class MetalPricesLambdaApp {
 
-   public static void main(final String[] args) {
-      App app = new App();
+    public static void main(final String[] args) {
+        App app = new App();
 
-      String accountId = Validations.requireNonEmpty(app, "accountId");
-      String region = Validations.requireNonEmpty(app, "region");
-      String environmentName = Validations.requireNonEmpty(app, "environmentName");
-      String applicationName = Validations.requireNonEmpty(app, "applicationName");
+        String accountId = Validations.requireNonEmpty(app, "accountId");
+        String region = Validations.requireNonEmpty(app, "region");
+        String environmentName = Validations.requireNonEmpty(app, "environmentName");
+        String applicationName = Validations.requireNonEmpty(app, "applicationName");
 
-      Environment awsEnvironment = CdkUtil.makeEnv(accountId, region);
+        Environment awsEnvironment = CdkUtil.makeEnv(accountId, region);
 
-      var appEnvironment = new ApplicationEnvironment(applicationName, environmentName);
+        var appEnvironment = new ApplicationEnvironment(applicationName, environmentName);
 
-      String lambdaStackName = CdkUtil.createStackName("lambda", appEnvironment);
+        String lambdaStackName = CdkUtil.createStackName("lambda", appEnvironment);
 
-      new MetalPricesLambdaStack(app, "MetalPricesLambdaStack",
-            StackProps.builder().stackName(lambdaStackName).env(awsEnvironment).build());
+        PolicyStatement allowSendingEmails = PolicyStatement.Builder.create()
+                .sid("AllowSendingEmails")
+                .effect(Effect.ALLOW)
+                .resources(List.of(
+                        "arn:aws:ses:%s:%s:configuration-set/my-first-configuration-set".formatted(region, accountId),
+                        "arn:aws:ses:%s:%s:identity/hugo.rad@gmail.com".formatted(region, accountId),
+                        "arn:aws:ses:%s:%s:identity/b-l-s.click".formatted(region, accountId)))
+                .actions(List.of("ses:SendEmail", "ses:SendRawEmail"))
+                .build();
 
-      PolicyStatement allowSendingEmails = PolicyStatement.Builder.create()
-            .sid("AllowSendingEmails")
-            .effect(Effect.ALLOW)
-            .resources(
-               List.of(
-                  "arn:aws:ses:%s:%s:configuration-set/my-first-configuration-set".formatted(region, accountId),
-                  "arn:aws:ses:%s:%s:identity/hugo.rad@gmail.com".formatted(region, accountId),
-                  "arn:aws:ses:%s:%s:identity/b-l-s.click".formatted(region, accountId)))
-            .actions(List.of("ses:SendEmail", "ses:SendRawEmail"))
-            .build();
+        new MetalPricesLambdaStack(
+                app,
+                "MetalPricesLambdaStack",
+                StackProps.builder()
+                        .stackName(lambdaStackName)
+                        .env(awsEnvironment)
+                        .build(),
+                List.of(allowSendingEmails));
 
-      app.synth();
-   }
+        app.synth();
+    }
 }
--- aws-cdk/src/main/java/com/serverless/lambda/cdk/MetalPricesLambdaStack.java
+++ aws-cdk/src/main/java/com/serverless/lambda/cdk/MetalPricesLambdaStack.java
@@ -1,23 +1,30 @@
 package com.serverless.lambda.cdk;
 
+import java.util.List;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.iam.PolicyStatement;
 import software.amazon.awscdk.services.lambda.Architecture;
 import software.amazon.awscdk.services.lambda.Function;
 import software.amazon.awscdk.services.lambda.Runtime;
+import software.amazon.awscdk.services.logs.RetentionDays;
 import software.constructs.Construct;
 
 public class MetalPricesLambdaStack extends Stack {
 
-    public MetalPricesLambdaStack(final Construct scope, final String id, final StackProps props) {
+    public MetalPricesLambdaStack(
+            Construct scope, String id, StackProps props, List<PolicyStatement> policyStatements) {
         super(scope, id, props);
 
-        Function.Builder.create(this, "metal-prices-lambda")
+        var function = Function.Builder.create(this, "metal-prices-lambda")
                 .runtime(Runtime.JAVA_21)
                 .code(software.amazon.awscdk.services.lambda.Code.fromAsset(
                         "../metal-prices-lambda/target/metal-prices-lambda-1.0.0-aws.jar"))
                 .handler("org.springframework.cloud.function.adapter.aws.FunctionInvoker::handleRequest")
                 .architecture(Architecture.X86_64)
+                .logRetention(RetentionDays.ONE_WEEK)
                 .build();
+
+        policyStatements.forEach(function::addToRolePolicy);
     }
 }
