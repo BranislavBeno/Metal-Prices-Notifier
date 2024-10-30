e7862d2a3e7e342e78aab33f52535c6b1cde28ae
Branislav Beno <brano.beno@gmail.com>
2024-10-30 20:53:29 +0100

    feat: Add explicitly defined log group

--- aws-cdk/src/main/java/com/serverless/lambda/cdk/MetalPricesLambdaStack.java
+++ aws-cdk/src/main/java/com/serverless/lambda/cdk/MetalPricesLambdaStack.java
@@ -10,6 +10,8 @@ import software.amazon.awscdk.services.iam.PolicyStatement;
 import software.amazon.awscdk.services.lambda.Architecture;
 import software.amazon.awscdk.services.lambda.Function;
 import software.amazon.awscdk.services.lambda.Runtime;
+import software.amazon.awscdk.services.logs.LogGroup;
+import software.amazon.awscdk.services.logs.LogGroupProps;
 import software.amazon.awscdk.services.logs.RetentionDays;
 import software.constructs.Construct;
 
@@ -17,11 +19,22 @@ import java.util.List;
 
 public class MetalPricesLambdaStack extends Stack {
 
+    public static final String FUNCTION_NAME = "metal-prices-lambda";
+
     public MetalPricesLambdaStack(
             Construct scope, String id, StackProps props, List<PolicyStatement> policyStatements) {
         super(scope, id, props);
 
-        var function = Function.Builder.create(this, "metal-prices-lambda")
+        var logGroup = new LogGroup(
+                this,
+                "MetalPricesLambdaLogGroup",
+                LogGroupProps.builder()
+                        .logGroupName("log-group-" + FUNCTION_NAME)
+                        .retention(RetentionDays.ONE_WEEK)
+                        .build());
+
+        var function = Function.Builder.create(this, "MetalPricesLambdaFunction")
+                .functionName(FUNCTION_NAME)
                 .runtime(Runtime.JAVA_21)
                 .code(software.amazon.awscdk.services.lambda.Code.fromAsset(
                         "../metal-prices-lambda/target/metal-prices-lambda-1.0.0-aws.jar"))
@@ -29,7 +42,7 @@ public class MetalPricesLambdaStack extends Stack {
                 .architecture(Architecture.X86_64)
                 .memorySize(512)
                 .timeout(Duration.seconds(15))
-                .logRetention(RetentionDays.ONE_WEEK)
+                .logGroup(logGroup)
                 .build();
 
         IManagedPolicy ssmReadOnlyAccess = ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMReadOnlyAccess");
